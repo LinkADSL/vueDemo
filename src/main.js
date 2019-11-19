f3d813ad50b3d8d05f9cbff16daf2100f883c2d3
LinkADSL <lan_stone@foxmail.com>
2019-11-20 01:54:14 +0800

    import antd

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Antd from 'ant-design-vue'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(Antd)
 
 /* eslint-disable no-new */
 new Vue({
